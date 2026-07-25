071a673bd5d237809f8de2c4035609a43ece2ac6
Morad <moradnaimi2009@gmail.com>
2026-07-25 22:58:06 +0100

    Fix: restore missing constants

--- app/src/main/java/com/lite/streamvault/util/Constants.kt
+++ app/src/main/java/com/lite/streamvault/util/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
     const val SUPABASE_URL = "https://gcxhdtjquboolttqdufq.supabase.co/"
     const val REST_BASE = "rest/v1/"
     const val SUPABASE_ANON_KEY = "sb_publishable_ueUz56XE625Kr46fGGpKvw_1g0avOWT"
+    const val BASE_URL = SUPABASE_URL
+    const val ADMOB_BANNER_ID = "ca-app-pub-3940256099942544/6300978111"
+    const val ADMOB_INTERSTITIAL_ID = "ca-app-pub-3940256099942544/1033173712"
 }
